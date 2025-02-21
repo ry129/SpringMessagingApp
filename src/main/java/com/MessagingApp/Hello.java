0260aa885cfe7cf48a12362ba6d45e6cf89d4de7
ry129 <0111cs211146.riya@gmail.com>
2025-02-21 22:21:41 +0530

    [Riya Yadav]Add.with name as parameter

--- src/main/java/com/MessagingApp/Hello.java
+++ src/main/java/com/MessagingApp/Hello.java
@@ -2,6 +2,7 @@ package com.MessagingApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class Hello {
     public String dsayHello(@RequestParam(value="name",defaultValue="Guest") String name) {
         return "Hello " + name + " from BridgeLabz !";
     }
+    @GetMapping("/helloo/param/{name}")
+    public String sayHello2(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz !";
+    }
+
 
 
 
