4005665dc8ba2941f79737f21ec1ac3fb15314b3
ry129 <0111cs211146.riya@gmail.com>
2025-02-21 21:52:34 +0530

    [Riya Yadav] Add. Hello by using Rest Controller

---
+++ src/test/java/com/MessagingApp/SpringMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MessagingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessagingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
